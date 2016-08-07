214725ee8443d2388ebb041a4cc015a44d0fedf0
Shelley <you@example.com>
2016-08-06 20:24:11 -0700

    still working on css

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ $(function () {
   $('a[title]').tooltip();
 });
 
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  e.target // newly activated tab
+  e.relatedTarget // previous active tab
+})
+
 $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?',
   function (data) {
     console.log(data);
