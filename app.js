f909ca3469fc6818a35a3811b57433913dfcfe7d
Shelley <you@example.com>
2016-08-10 14:16:55 -0700

    added shell all pages

--- app.js
+++ app.js
@@ -13,21 +13,9 @@ $(".users").click(function () {
   $("#status").hide();
 });
 
-$("#close-1").click(function () {
+$("#close").click(function () {
   $("#all-users").hide();
   $("#status").show();
-});
-
-$("#online-users").hide();
-
-$(".show-on-users").click(function () {
-  $("#online-users").show();
-  $("#status-on").hide();
-});
-
-$("#close-2").click(function () {
-  $("#online-users").hide();
-  $("#status-on").show();
 });
 
 $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?',
