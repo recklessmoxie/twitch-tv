c53f828168618d72a419a1b1342d934172152da2
Shelley <you@example.com>
2016-08-11 14:36:12 -0700

    fixing the headache

--- app.js
+++ app.js
@@ -24,15 +24,31 @@ $("#close").click(function () {
 
 $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?',
   function (data) {
+    var preview = data.stream.channel.game;
+    var playerId = data.stream.channel._id;
+
+    html +=
+      '<script src= "http://player.twitch.tv/js/embed/v1.js"></script><div id="{PLAYER_DIV_ID}"></div><script type="text/javascript">var options = {width: 854, height: 480,channel: "' +
+      playerId +
+      '"}; var player = new Twitch.Player("{PLAYER_DIV_ID}", options); player.setVolume(0.5);</script>';
+    $('#all-users').append(html);
+    console.log(preview);
     console.log(data);
   });
 
 $.getJSON('https://api.twitch.tv/kraken/channels/freecodecamp?callback=?',
   function (data) {
     var userLogo = data.logo;
+    var streamingContent = data.status;
+    var userName = data.display_name;
+    var contentUrl = data.url;
 
     html += '<img class="img-fluid card-img-top"' + 'src="' + userLogo +
-      '" alt=""><div class="card-block"><h4 class="card-title text-xs-center">Card title</h4><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p><a href="#" class="btn btn-primary">Go somewhere</a></div>';
+      '" alt=""><div class="card-block"><h4 class="card-title text-xs-center">' +
+      userName +
+      '</h4><p class="card-text">' + streamingContent + '</p><a href="' +
+      contentUrl + '" class="btn btn-primary">View Channel</a></div>';
 
     $('#all-users').append(html);
+    console.log(data);
   });
